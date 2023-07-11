0e5071f8445cb0502d26edf934ad18da287a0927
Wunna Kyaw <heronskyaw@gmail.com>
2023-07-12 00:05:58 +0630

    first initial commit

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,14 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var messageString = "You Are Great!";
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundColor(.red)
+                .padding()
+            
+            Button("Press Me") {
+                // This is the action performed when the button is pressed
+                messageString = "You Are Awesome!"
+            }
         }
-        .padding()
     }
 }
 
