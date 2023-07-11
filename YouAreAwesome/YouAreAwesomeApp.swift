bce65b4f1a866b0ad51304b5f520e3909a9c034b
Wunna Kyaw <heronskyaw@gmail.com>
2023-07-11 11:15:39 +0630

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Wunna Kyaw on 7/11/23.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
